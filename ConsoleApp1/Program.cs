9459ee9c6c26e1b8fe04726b624d0d7c371d1d51
FernandoRFer <fernandoproc@outlook.com>
2022-06-09 12:05:13 -0300

    Teste

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,109 @@
+﻿using ConsoleApp1;
+
+
+
+
+string I = null;
+string IIb = null;
+string II = null;
+string IIIb = null;
+string III = null;
+string IV = null;
+
+
+
+string tom = Console.ReadLine();
+
+
+var sol = new TomG();
+var fa = new TomF();
+var C = new TomC();
+
+if (tom == "F")
+{
+     I = fa.I;
+    II = fa.II;
+    III = fa.III;   
+
+}
+
+if (tom == "G")
+{
+    I = sol.I;
+    II = sol.II;
+    III = sol.III;
+
+}
+
+
+if (tom == "C")
+{
+    I = C.I;
+    II = C.II;
+    III = C.III;
+
+}
+
+
+string musicb = (" | " +I+"7+(9)/"+III + " | " + I +" | " + III+"m"+" | " + II+"m" + " | " + "\n"+ " | " + I + " " +  II + "m" + " | " + I + " | " + III + "m" + " | " + II + "m" + " | " );
+
+// var n = musicb.Replace(" II ", II ) ;
+
+
+
+
+
+
+
+Console.WriteLine(musicb);
+
+
+
+public class TomF
+{
+    public string T = "F";
+    public string I = "F";
+    public string IIb = "Gb";
+    public string II = "G";
+    public string IIIb = "Ab";
+    public string III = "A";
+
+    public void AplicaTom()
+    {
+        var fa = new TomF();
+
+          I = fa.I;
+    II = fa.II;
+    III = fa.III;  
+
+    }
+
+    
+}
+
+
+
+public class TomG
+{
+    public string T = "G";
+    public string I = "G";
+    public string IIb = "Ab";
+    public string II = "A";
+    public string IIIb = "Bb";
+    public string III = "B";
+}
+
+
+public class TomC
+{
+    public string T = "C";
+    public string I = "C";
+    public string IIb = "Db";
+    public string II = "D";
+    public string IIIb = "Eb";
+    public string III = "E";
+}
+
+
+
+
---
+++ ConsoleApp1/Tons.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Tons
+    {
+        public string TomName { get; set; }
+        public string I { get; set; }
+        public string IIb { get; set; }
+        public string II { get; set; }
+        public string III { get; set; }
+        public string IIIb { get; set; }
+        public string IV { get; set; }
+        public string Vb { get; set; }
+        public string V { get; set; }
+        public string VIb { get; set; }
+        public string VI { get; set; }
+        public string VIIb { get; set; }
+        public string VII { get; set; }
+
+
+     
+
+
+
+
+        /*
+        public void TodosTom()
+        {
+            List<Tons> tom = new List<Tons>()
+        {
+                new Tons()
+                {
+                    TomName = "C",
+                    Notas = new List<string> { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" }
+                 },
+                 new Tons()
+                 {
+                    TomName = "D",
+                    Notas = new List<string> { "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#" }
+                 },
+                  new Tons()
+                  {
+                    TomName = "E",
+                    Notas = new List<string> { "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#" }
+                  }
+
+
+
+
+            // List<string> C = new List<string> { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
+            // List<string> D = new List<string> { "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#" };
+
+            // List<string> E = new List<string> { "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#" };
+
+            // List<string> F = new List<string> { "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E" };
+        };*/
+
+        
+    }
+}
